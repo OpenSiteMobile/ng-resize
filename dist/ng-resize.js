e1f1e6b7a01413af453502be7f08b382db5335e1
Dwight Vietzke Jr <dwightvietzke@gmail.com>
2016-03-07 13:59:40 -0500

    jsLint compatable version of ng-resize.js
    Minor tweaks to pass most jsLint ckecks.

--- dist/ng-resize.js
+++ dist/ng-resize.js
@@ -3,7 +3,9 @@
 * (c) 2014 Daniel Smith http://www.danmasta.com
 * License: MIT
 */
-(function(window, angular, undefined){
+
+(function (angular) {
+	"use strict";
 
 	/**
 	* @name ngResize
@@ -30,90 +32,102 @@
 	* $broadcasts 'resize' event from $rootScope
 	* which gets inherited by all child scopes
 	*/
-	ngResize.provider('resize', [function resizeProvider(){
+	ngResize.provider(
+		'resize',
+		[function resizeProvider() {
 
-		// store throttle time
-		this.throttle = 100;
+			// store throttle time
+			this.throttle = 100;
 
-		// by default bind window resize event when service
-		// is initialize/ injected for first time
-		this.initBind = 1;
+			// by default bind window resize event when service
+			// is initialize/ injected for first time
+			this.initBind = 1;
 
-		// service object
-		this.$get = ['$rootScope', '$window', '$interval',  function($rootScope, $window, $interval){
+			// service object
+			this.$get = ['$rootScope', '$window', '$interval',  function ($rootScope, $window, $interval) {
+				var _this = this,
+					bound = 0,
+					timer = 0,
+					resized = 0;
 
-			var _this = this;
+				// api to set throttle amount
+				function setThrottle(throttle) {
+					_this.throttle = throttle;
+				}
 
-			// api to set throttle amount
-			function setThrottle(throttle){
-				_this.throttle = throttle
-			};
+				// api to get current throttle amount
+				function getThrottle() {
+					return _this.throttle;
+				}
 
-			// api to get current throttle amount
-			function getThrottle(){
-				return _this.throttle;
-			};
+				// trigger a resize event on provided $scope or $rootScope
+				function trigger($scope) {
+					$scope = $scope || $rootScope;
 
-			// trigger a resize event on provided $scope or $rootScope
-			function trigger($scope){
-				var $scope = $scope || $rootScope;
-				$scope.$broadcast('resize', {
-					width: $window.innerWidth,
-					height: $window.innerHeight
-				});
-			};
-
-			// bind to window resize event, will only ever be bound
-			// one time for entire app
-			var bound = 0;
-			var timer = 0;
-			var resized = 0;
-			function bind(){
-				if(!bound){
-					var w = angular.element($window);
-					w.on('resize', function(event){
-						if(!resized){
-							timer = $interval(function(){
-								if(resized){
-									resized = 0;
-									$interval.cancel(timer);
-									trigger();
-								}
-							}, _this.throttle);
+					$scope.$broadcast(
+						'resize',
+						{
+							width: $window.innerWidth,
+							height: $window.innerHeight
 						}
-						resized = 1;
-					});
-					bound = 1;
-					w.triggerHandler('resize');
+					);
 				}
-			};
 
-			// unbind window scroll event
-			function unbind(){
-				if(bound){
-					var w = angular.element($window);
-					w.off('resize');
-					bound = 0;
+				// bind to window resize event, will only ever be bound
+				// one time for entire app
+				function bind() {
+					if(!bound){
+						var w = angular.element($window);
+	
+						w.on(
+							'resize',
+							function () {
+								if (!resized) {
+									timer = $interval(
+										function () {
+											if (resized) {
+												resized = 0;
+												$interval.cancel(timer);
+												trigger();
+											}
+										},
+										_this.throttle
+									);
+								}
+	
+								resized = 1;
+							}
+						);
+
+						bound = 1;
+						w.triggerHandler('resize');
+					}
 				}
-			};
 
-			// by default bind resize event when service is created
-			if(_this.initBind){
-				bind();
-			}
+				// unbind window scroll event
+				function unbind() {
+					if (bound) {
+						var w = angular.element($window);
 
-			// return api
-			return{
-				getThrottle: getThrottle,
-				setThrottle: setThrottle,
-				trigger: trigger,
-				bind: bind,
-				unbind: unbind
-			};
+						w.off('resize');
+						bound = 0;
+					}
+				}
 
-		}];
+				// by default bind resize event when service is created
+				if (_this.initBind) { bind(); }
 
-	}]);
+				// return api
+				return {
+					getThrottle: getThrottle,
+					setThrottle: setThrottle,
+					trigger: trigger,
+					bind: bind,
+					unbind: unbind
+				};
+			}];
+		}]
+	);
 
 	/*
 	* ngResize Directive
@@ -127,21 +141,32 @@
 	* $timeout is used to debounce any expressions
 	* to the end of the current digest
 	*/
-	ngResize.directive('ngResize', ['$parse', '$timeout', 'resize', function($parse, $timeout, resize){
-		return {
-			compile: function($element, attr){
-				var fn = $parse(attr['ngResize']);
-				return function(scope, element, attr){
-					scope.$on('resize', function(event, data){
-						$timeout(function(){
-							scope.$apply(function(){
-								fn(scope, { $event: data });
-							});
-						});
-					});
-				};
-			}
-		};
-	}]);
+	ngResize.directive(
+		'ngResize',
+		['$parse', '$timeout', function ($parse, $timeout) {
+			return {
+				compile: function ($element, attr) {
+					var fn = $parse(attr.ngResize);
 
-})(window, window.angular);
+					return function (scope) {
+						scope.$on(
+							'resize',
+							function (event, data) {
+								$timeout(
+									function () {
+										scope.$apply(
+											function () {
+												fn(scope, { $event: data });
+											}
+										);
+									}
+								);
+							}
+						);
+					};
+				}
+			};
+		}]
+	);
+
+}(window.angular));
